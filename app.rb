a722b06dd3b51e83a84e368da42fcea3fc6a7da8
Kelin Field <kfield111@hotmail.com>
2020-10-25 16:53:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ class App < Sinatra::Base
 
   post '/team' do
 
-    @params[:name]
-    @params[:coach]
-    @params[:pg]
-    @params[:sg]
-    @params[:sf]
-    @params[:pf]
-    @params[:c]
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
 
     erb :team
   end
